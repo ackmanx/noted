dab9a4b18e95b5b68dd82ebdae18b1b5894daeb9
ackmanx <ackmanx@users.noreply.github.com>
2023-12-10 13:58:31 -0600

    Update config so intellij can format svelte files

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -4,9 +4,8 @@ module.exports = {
   trailingComma: 'es5',
   singleQuote: true,
   printWidth: 80,
-  // Once Intellij can handle the prettier svelte plugin, I can upgrade to prettier v3
-  // Breaking change for that is requiring a plugins array vs v2 where it autodetects
-  // plugins: ['prettier-plugin-svelte', '@trivago/prettier-plugin-sort-imports'],
+  // Intellij cannot format Svelte files with prettier without explicitly referencing the plugin
+  plugins: ['prettier-plugin-svelte', '@trivago/prettier-plugin-sort-imports'],
   svelteSortOrder: 'options-scripts-styles-markup',
   // trivago/prettier-plugin-sort-imports
   importOrder: ['<THIRD_PARTY_MODULES>', '^[@]', '^[$]', '^[./]'],
