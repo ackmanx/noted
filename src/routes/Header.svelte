7eb2fac784dc9f8239ec9f0b45304e603c959325
ackmanx <ackmanx@users.noreply.github.com>
2023-12-11 14:27:24 -0600

    Add toggle icons

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,28 +1,55 @@
 <script>
+  import PreviewIcon from '$lib/images/preview-icon.svg'
+  import EditIcon from '$lib/images/edit-icon.svg'
+
   /** @type boolean */
   export let isPreviewActive
 </script>
 
 <style>
   header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
     font-family: 'cabin sketch', sans-serif;
     font-size: 40px;
     font-style: italic;
   }
+
+  .icons-container {
+    display: flex;
+    align-items: center;
+  }
+
+  button {
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+  }
+
+  img {
+    height: 40px;
+  }
 </style>
 
 <header>
-  noted
+  <div>noted</div>
 
-  <button
-    on:click={() => {
-      isPreviewActive = !isPreviewActive
-    }}
-  >
-    {#if isPreviewActive}
-      Edit
-    {:else}
-      Preview
-    {/if}
-  </button>
+  <div class="icons-container">
+    <button
+      on:click={() => {
+        isPreviewActive = false
+      }}
+    >
+      <img src={EditIcon} alt="" />
+    </button>
+
+    <button
+      on:click={() => {
+        isPreviewActive = true
+      }}
+    >
+      <img src={PreviewIcon} alt="" />
+    </button>
+  </div>
 </header>
