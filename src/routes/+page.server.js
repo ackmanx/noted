e37966118652d0b6d314f92065e5495274cb5225
ackmanx <ackmanx@users.noreply.github.com>
2023-12-16 20:30:22 -0600

    Starting file tree

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,15 +1,22 @@
 import { notes } from '$lib/db/notes.js'
 
 export const load = async function () {
-  const data = await notes
+  const documentsForUser = await notes
     .find({ author: 'ackmanx' })
-    .project({ _id: 1, path: 1 })
+    .project({ _id: 1, path: 1, label: 1 })
     .toArray()
 
   // Debug logging
-  console.log('^_^', 'App data from MongoDB:', data)
+  console.log('^_^', 'Documents from MongoDB:', documentsForUser)
+
+  const fileTree = documentsForUser.map((document) => ({
+    ...document,
+    _id: String(document._id),
+  }))
+
+  /* prettier-ignore */ console.log('^_^', 'File tree:', fileTree)
 
   return {
-    text: data,
+    fileTree,
   }
 }
