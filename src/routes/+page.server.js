a82256bef4e46dd9b7414616a582b6896a8746b8
ackmanx <ackmanx@users.noreply.github.com>
2023-12-16 18:24:04 -0600

    Change mongo query

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,10 @@
 import { notes } from '$lib/db/notes.js'
 
 export const load = async function () {
-  const data = await notes.find({}).toArray()
+  const data = await notes
+    .find({ author: 'ackmanx' })
+    .project({ _id: 1, path: 1 })
+    .toArray()
 
   // Debug logging
   console.log('^_^', 'App data from MongoDB:', data)
