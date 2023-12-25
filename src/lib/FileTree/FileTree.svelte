ab82fa473da7c992ccaac3dd5ccb20202e857ebd
ackmanx <ackmanx@users.noreply.github.com>
2023-12-25 08:53:03 -0600

    Add my own tree component

---
+++ src/lib/FileTree/FileTree.svelte
@@ -0,0 +1,19 @@
+<script>
+  import TreeItem from '$lib/FileTree/TreeItem.svelte'
+
+  /** @type TreeViewNode[] */
+  export let fileTree
+
+  export let onClickLeaf
+</script>
+
+<style>
+</style>
+
+<section>
+  <ul>
+    {#each fileTree as treeItem}
+      <TreeItem {...treeItem} {onClickLeaf} />
+    {/each}
+  </ul>
+</section>
