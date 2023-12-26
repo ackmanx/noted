cd5a7f01d97d899fda5e931a5ea38c01052d6417
ackmanx <ackmanx@users.noreply.github.com>
2023-12-25 22:43:03 -0600

    basic skeleton menu poc

--- src/routes/FolderView.svelte
+++ src/routes/FolderView.svelte
@@ -8,6 +8,9 @@
   /** @type string[] */
   export let foldersIdList
 
+  let isContextMenuOpen = false
+  let mouseXY = {}
+
   async function handleFetchMarkdown({ detail }) {
     if (foldersIdList.includes(detail.id)) {
       return
@@ -18,16 +21,46 @@
 
     markdownInput = json.markdown
   }
+
+  function handleRightClick(event) {
+    event.preventDefault()
+
+    mouseXY.x = event.clientX
+    mouseXY.y = event.clientY
+
+    isContextMenuOpen = !isContextMenuOpen
+  }
 </script>
 
 <style>
   .folder-view {
     width: 300px;
+    min-height: 100vh;
     flex-shrink: 0;
     margin-right: 10px;
   }
+
+  .context-menu {
+    position: absolute;
+    top: var(--mouseY);
+    left: var(--mouseX);
+    background-color: white;
+    color: black;
+  }
 </style>
 
-<div class="folder-view">
+<div
+  class="folder-view"
+  on:click={handleRightClick}
+  on:contextmenu={handleRightClick}
+>
+  {#if isContextMenuOpen}
+    <div
+      class="context-menu"
+      style="--mouseX: {mouseXY.x}px; --mouseY: {mouseXY.y}px;"
+    >
+      hello there
+    </div>
+  {/if}
   <FileTree {fileTree} onClickLeaf={handleFetchMarkdown} />
 </div>
