1c9b8b5142ed40f7632d2d103d71b4d7ee5d45b1
ackmanx <ackmanx@users.noreply.github.com>
2023-12-29 15:32:28 -0600

    Update a note

--- src/routes/api/note/[id]/+server.js
+++ src/routes/api/note/[id]/+server.js
@@ -3,8 +3,16 @@ import { json } from '@sveltejs/kit'
 import { notes } from '$lib/db/notes.js'
 
 export async function POST({ request, params }) {
-  const body = await request.json()
+  const noteInDB = await notes.findOne({ _id: params.id })
 
-  const note = await notes.findOne({ _id: params.id })
-  return json(note)
+  noteInDB.markdown = (await request.json()).markdown
+
+  await notes.updateOne(
+    { _id: params.id },
+    {
+      $set: noteInDB,
+    }
+  )
+
+  return json({ success: true })
 }
