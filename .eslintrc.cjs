7ee4327004f721e1491f8404157950f1704f2967
ackmanx <ackmanx@users.noreply.github.com>
2024-01-01 16:01:10 -0600

    eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,12 @@
 module.exports = {
   root: true,
-  extends: ['eslint:recommended', 'plugin:svelte/recommended', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:svelte/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint'],
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2020,
