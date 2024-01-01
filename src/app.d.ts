b18fac78cb8fdd09022eb94a80abe7b0d5ff0d79
ackmanx <ackmanx@users.noreply.github.com>
2023-12-31 22:23:31 -0600

    Add types to $page.data

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,16 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+import { Note } from '$lib/db/notes'
+
 declare global {
   namespace App {
     // interface Error {}
     // interface Locals {}
-    // interface PageData {}
+    interface PageData {
+      foldersIdList: string[]
+      fileTree: any
+      note: Note
+    }
     // interface Platform {}
   }
 }
