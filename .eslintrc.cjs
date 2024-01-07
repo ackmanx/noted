63d602a9876cd6986ecfb03b36979dd2187260d1
ackmanx <ackmanx@users.noreply.github.com>
2024-01-06 23:03:47 -0600

    Eslint configuration to parse typescript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,9 @@
+/*
+ * ESLint will fail to compile TypeScript unless configured to do so
+ * - We add the typescript eslint plugin
+ * - We add the typescript eslint parser
+ * - We add override config to treat Svelte files with TS as such
+ */
 module.exports = {
   root: true,
   extends: [
@@ -7,6 +13,7 @@ module.exports = {
     'prettier',
   ],
   plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2020,
@@ -17,4 +24,14 @@ module.exports = {
     es2017: true,
     node: true,
   },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      // Parse the `<script>` in `.svelte` as TypeScript by adding the following configuration.
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+    },
+  ]
 }
